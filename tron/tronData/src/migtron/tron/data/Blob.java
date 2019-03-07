67252b9208ec8ac89681b31f56d46e933e3195d9
Albarral <albarral@migtron.com>
2019-03-07 13:34:41 +0100

    tron: new tron.data package

---
+++ tron/tronData/src/migtron/tron/data/Blob.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright (C) 2019 by Migtron Robotics   
+ *  albarral@migtron.com
+ */
+package migtron.tron.data;
+
+import java.awt.geom.Point2D.Float;
+import java.awt.Point;
+
+import migtron.tron.util.math.Ellipse;
+import migtron.tron.util.math.Vec3f;
+
+/**
+* This class represents a blob, the basic visual entity in the tron system.
+* A blob is represented by an ellipse with a mass.
+* @author albarral
+ */
+
+public class Blob extends Ellipse implements Cloneable
+{
+    protected int mass;                  // area in pixels
+    protected float shapeFactor;      // automatically computed  
+    
+    public Blob(Point pos, Vec3f covs, int mass)
+    {
+        super(new Float(pos.x, pos.y), covs);
+        this.mass = mass;    
+        updateShapeFactor();        
+    }    
+
+    public Blob()
+    {
+        this(new Point(0, 0), new Vec3f(0, 0, 0), 0);
+    }
+
+    public Blob(Blob blob)
+    {
+        this(blob.getPointPosition(), blob.getCovariances(), blob.getMass());
+    }    
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException 
+    {
+        return (Blob)super.clone();
+    }
+    
+    public int getMass() {return mass;};
+    public float getShapeFactor() {return shapeFactor;};    
+    public void setMass(int value) {mass = value;};
+
+    // automatic computation of the shape factor from the ellipse main axes
+    protected void updateShapeFactor()
+    {
+        shapeFactor = computeShapeFactor();        
+    }
+    
+    // merge this blob with another blob
+    public void merge(Blob blob2)
+    {
+        int newMass = mass + blob2.getMass();
+        if (newMass != 0)
+        {
+            float w1 = (float)mass / newMass;
+            float w2 = (float)mass / newMass;
+            // main axes are internally recomputed
+            super.mergeEllipse(blob2, w1, w2);
+            mass = newMass;      
+            updateShapeFactor();
+        }
+    }
+    
+    @Override
+    public void clear()
+    {
+        super.clear();
+        mass = 0;
+        shapeFactor = 0f; 
+    }
+
+    @Override
+    public String toString()
+    {
+        String desc = "Blob [mass = " + String.valueOf(mass) + ", shape_factor = " + String.valueOf(shapeFactor) + "] " + super.toString();
+        return desc;
+    }
+
+    public String shortDesc()
+    {
+        String desc = "Blob [mass = " + String.valueOf(mass) + ", pos = (" + String.valueOf(pos.x) + "," + String.valueOf(pos.y) + ")]";
+        return desc;
+    }
+}
---
+++ tron/tronData/src/migtron/tron/data/ColorBlob.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright (C) 2019 by Migtron Robotics   
+ *  albarral@migtron.com
+ */
+package migtron.tron.data;
+
+import java.awt.Point;
+import migtron.tron.util.color.RGBColor;
+import migtron.tron.util.math.Vec3f;
+
+/**
+* This class represents a colored blob.
+* @author albarral
+ */
+
+public class ColorBlob extends Blob implements Cloneable
+{
+    protected Vec3f rgbColor;     // color in RGB space
+    protected Vec3f hsvColor;     // color in HSV space (automatically computed)
+    
+    public ColorBlob(Point pos, Vec3f covs, int mass, Vec3f rgbColor)
+    {
+        super(pos, covs, mass);
+        this.rgbColor = new Vec3f(rgbColor);
+        updateHSVColor();
+    }    
+    
+//    public ColorBlob()
+//    {
+//        this(new Point(0, 0), 
+//                new Vec3f(0, 0, 0), 
+//                0, 
+//                new Vec3f(0, 0, 0));
+//    }
+
+    public ColorBlob(ColorBlob colorBlob)
+    {
+        this(colorBlob.getPointPosition(), 
+                colorBlob.getCovariances(), 
+                colorBlob.getMass(), 
+                colorBlob.getRGB());
+    }    
+
+    @Override
+    public Object clone() throws CloneNotSupportedException 
+    {
+        ColorBlob cloned = (ColorBlob)super.clone();
+        cloned.rgbColor = (Vec3f)rgbColor.clone();
+        cloned.hsvColor = (Vec3f)hsvColor.clone();
+        return cloned;
+    }
+
+    public Vec3f getRGB() {return rgbColor;};
+    public Vec3f getHSV() {return hsvColor;};    
+    // set the rgb color (the hsv color is automatically updated)
+    public void setRGB(Vec3f color) 
+    {
+        rgbColor.assign(color);
+        updateHSVColor();
+    }
+
+    // automatic computation of the HSV color from the RGB color
+    private void updateHSVColor()
+    {
+        hsvColor.assign(RGBColor.toHSV(rgbColor));       
+    }
+
+    // merge this color blob with another color blob
+    public void merge(ColorBlob colorBlob2)
+    {
+        // merge blobs
+        super.merge(colorBlob2);
+        // merge colors
+        Vec3f newRGB = RGBColor.mergeValues(rgbColor, colorBlob2.getRGB(), mass, colorBlob2.getMass());
+        // update rgb (hsv automatically updated)
+        setRGB(newRGB);            
+    }
+                  
+    @Override
+    public String toString()
+    {
+        String desc = "ColorBlob [rgb = " + rgbColor.toString() + ", hsv = " + hsvColor.toString() + "] " + super.toString();
+        return desc;
+    }
+
+    @Override
+    public String shortDesc()
+    {
+        String desc = "ColorBlob [hsv = " + hsvColor.toString() + "] " + super.shortDesc();
+        return desc;
+    }
+}
